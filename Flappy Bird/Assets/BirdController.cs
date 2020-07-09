ab88867cfea8006e0364474cca4f2b243a554b92
VitoSenic <67893442+VitoSenic@users.noreply.github.com>
2020-07-09 14:45:13 +0200

    player-movement

---
+++ Flappy Bird/Assets/BirdController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BirdController : MonoBehaviour
+{
+    public float flyvelocity = 400f;
+    private Rigidbody2D body;
+    private Vector3 initPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        initPosition = transform.position;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            body.velocity = Vector2.zero;
+            body.AddForce(new Vector2(0, flyvelocity));
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "Danger"){
+            reset();
+        }
+    }
+    void reset() {
+        transform.position = initPosition;
+    }
+}
