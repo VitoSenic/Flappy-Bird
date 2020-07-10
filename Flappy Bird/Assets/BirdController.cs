0472e49e0d9dd6e2b4647f8481159fa066d56943
VitoSenic <67893442+VitoSenic@users.noreply.github.com>
2020-07-10 10:51:37 +0200

    pipe.movement&score

--- Flappy Bird/Assets/BirdController.cs
+++ Flappy Bird/Assets/BirdController.cs
@@ -1,21 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class BirdController : MonoBehaviour
 {
     public float flyvelocity = 400f;
     private Rigidbody2D body;
     private Vector3 initPosition;
+     private int score = 0;
+     public GameObject scoreboard;
+     private bool isPaused = false;
+     public Button startButton;
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
         initPosition = transform.position;
+        scoreboard.SetActive(false);
+        Button btn = startButton.GetComponent<Button>();
+		btn.onClick.AddListener(startGame);
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.V))
+        if (Input.GetKeyDown(KeyCode.V)|| Input.GetMouseButtonDown(0))
         {
             body.velocity = Vector2.zero;
             body.AddForce(new Vector2(0, flyvelocity));
@@ -28,6 +37,32 @@ public class BirdController : MonoBehaviour
         }
     }
     void reset() {
-        transform.position = initPosition;
+        if (ScoreTrack.bestscore < score) {
+            ScoreTrack.bestscore  = score;
+        }
+        Time.timeScale = 0f;
+        scoreboard.SetActive(true);
+        isPaused = true;
+            GameObject.FindWithTag("cScore")
+        .GetComponent<UnityEngine.UI.Text>().text = score + "";
+        GameObject.FindWithTag("bScore")
+        .GetComponent<UnityEngine.UI.Text>().text = ScoreTrack.bestscore  + "";
     }
+    void startGame() {
+        isPaused = false;
+        Time.timeScale =1f;
+        transform.position = initPosition;
+        SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+    }
+    public void updateScore(int amount) {
+        score += amount;
+        GameObject.FindWithTag("ScoreText")
+            .GetComponent<UnityEngine.UI.Text>().text = score + "";
+    }
+    
+}
+
+public class ScoreTrack
+{
+    public static int bestscore = 0;
 }
--- Flappy Bird/Assets/spawner.cs
+++ Flappy Bird/Assets/spawner.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class spawner : MonoBehaviour
 {
     public GameObject pipePrefab;
+
+    float minY = -0.5f;
+    float maxY = 2.5f;
+    float delay = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,11 @@ public class spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        delay -= Time.deltaTime;
+         
+        if (delay < 0){
+            delay = 2f;
+            Instantiate(pipePrefab, new Vector3(3, Random.Range(minY, maxY), 0), Quaternion.identity);
+        }
+   }
 }
